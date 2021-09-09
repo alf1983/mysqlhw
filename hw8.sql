b66559371a836be4193195e3740e78b8eed8649a
Vasily <berkach@gmail.com>
2021-09-09 17:35:26 +0300

    Homework #8

---
+++ hw8.sql
@@ -0,0 +1,44 @@
+-- Упражнение 1
+	SELECT
+		users.id,
+		CONCAT(users.first_name, " ", users.last_name) AS user_full_name,
+		COUNT(*) AS mesages_count
+	FROM messages
+	INNER JOIN users ON
+		messages.from_user_id = users.id
+	AND
+		messages.to_user_id = 5
+	GROUP BY from_user_id
+	ORDER BY mesages_count
+	DESC LIMIT 1;
+
+-- Упражнение 2
+	SELECT COUNT(*) AS likes_count
+		FROM posts_likes
+		INNER JOIN posts
+			ON posts.id = posts_likes.post_id
+		JOIN profiles
+			ON profiles.user_id = posts.user_id
+		AND TIMESTAMPDIFF(YEAR, profiles.birthday, NOW()) < 10;
+
+-- Упражнение 3
+	SELECT
+		IF(
+			(
+				SELECT COUNT(*) FROM posts_likes
+				JOIN profiles
+				ON
+					profiles.user_id = posts_likes.user_id
+				AND
+					profiles.gender = 'm'
+			) > (
+				SELECT COUNT(*) FROM posts_likes
+				JOIN profiles
+				ON
+					profiles.user_id = posts_likes.user_id
+				AND
+					profiles.gender = 'f'
+			),
+			'Мужчины поставили больше лайков',
+			'Женщины поставили больше лайков'
+		) AS сomparison;
